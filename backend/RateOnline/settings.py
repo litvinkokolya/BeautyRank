d261873710affea8bad3ae70cc927c3d999e0e25
lomkahome <mrlomka407@mail.ru>
2024-09-01 22:20:49 +0300

    repeat for action

--- backend/RateOnline/settings.py
+++ backend/RateOnline/settings.py
@@ -7,6 +7,7 @@ import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
+
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 SECRET_KEY = env("SECRET_KEY", default="super-secret-key")
